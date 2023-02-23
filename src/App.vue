eecdb18cf43aaa7688044a682be829eaae4a4a45
Administrator <admin@example.com>
2023-02-23 11:28:04 +0800

    App.vue 样式

--- src/App.vue
+++ src/App.vue
@@ -19,9 +19,9 @@ export default {
   color: #484a4d;
   /* background:  #b6bbbb21; */
   position: absolute;
-  top: 0px;
-  bottom: 0px;
-  left: 0px;
-  right: 0px;
+  top: 20px;
+  bottom: 20px;
+  left: 20px;
+  right: 20px;
 }
 </style>
